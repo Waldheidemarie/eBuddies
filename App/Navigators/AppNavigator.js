e19125d4970a2a87e7fe7303dfe229b28ec38583
Qi Lin <qlin1996@gmail.com>
2020-08-21 15:43:04 -0400

    removed plist

--- App/Navigators/AppNavigator.js
+++ App/Navigators/AppNavigator.js
@@ -3,8 +3,6 @@ import { createAppContainer, createStackNavigator } from 'react-navigation'
 import ExampleScreen from 'App/Containers/Example/ExampleScreen'
 import SplashScreen from 'App/Containers/SplashScreen/SplashScreen'
 
-import UserProfileScreen from 'App/Containers/UserProfile/UserProfileScreen'
-
 /**
  * The root screen contains the application's navigation.
  *
@@ -17,8 +15,7 @@ const StackNavigator = createStackNavigator(
     SplashScreen: SplashScreen,
     // The main application screen is our "ExampleScreen". Feel free to replace it with your
     // own screen and remove the example.
-    // MainScreen: ExampleScreen,
-    MainScreen: UserProfileScreen,
+    MainScreen: ExampleScreen,
   },
   {
     // By default the application will show the splash screen

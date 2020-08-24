22bdaf864460a6434ba49354b1af3f75c2170a77
Blake Ferm <blakeferm@gmail.com>
2020-08-24 13:10:08 -0400

    updating navbar - in progress

--- App/Navigators/AppNavigator.js
+++ App/Navigators/AppNavigator.js
@@ -14,16 +14,22 @@ const StackNavigator = createStackNavigator(
   {
     // Create the application routes here (the key is the route name, the value is the target screen)
     // See https://reactnavigation.org/docs/en/stack-navigator.html#routeconfigs
-    // SplashScreen: SplashScreen,
     // The main application screen is our "ExampleScreen". Feel free to replace it with your
     // own screen and remove the example.
-    MainScreen: SingleEventScreen,
+    SplashScreen: SplashScreen,
+    Login: LoginScreen,
+    SignUp: SignUpScreen,
+    Interests: InterestsScreen,
+    Events: AllEventsScreen,
+    SingleEvent: SingleEventScreen,
+    UserProfile: UserProfileScreen,
+    Chat: ChatScreen,
   },
   {
     // By default the application will show the splash screen
     initialRouteName: SplashScreen,
     // See https://reactnavigation.org/docs/en/stack-navigator.html#stacknavigatorconfig
-    headerMode: 'none',
+    headerMode: 'screen ',
   }
 )
 
